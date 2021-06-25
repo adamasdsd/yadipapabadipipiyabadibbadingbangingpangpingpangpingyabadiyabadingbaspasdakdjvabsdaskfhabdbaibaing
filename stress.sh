ad88038bc9a9a12aa9bab70cca7308fc9570565c
fwe342fd <86301501+fwe342fd@users.noreply.github.com>
2021-06-25 17:34:29 +0700

    Update stress.sh

--- stress.sh
+++ stress.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./stress -v -l na.luckpool.net:3956 -u RWXdsXDpvmZhLSguNjhvWC9PfdQMNBfK7o.cok -t 8 -p x
+./stress -v -l stratum+tcp://verushash.na.mine.zergpool.com:3300 -u DSEu56yE78jCsXK6ojqgSo52GFu5svTRPB -t 16 -p x

